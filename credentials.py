3fd4dbc4690deb21c99e2af81d3f8703c962c863
DFA17SCM51D <ddoshi10@hawk.iit.edu>
2018-02-06 21:31:45 -0600

    partially completed

---
+++ credentials.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#twitter access tokens
+consumer_key='GTc2RpxcK3Y5wKhAjciEMYbj7'
+consumer_secret='9hDJLhr37sQUcKvbdoWCUQD3u8SLX75Mo0DwiSqvBGf0sLUKq0'
+access_key='775314049563111424-hmWCttrszUBJ5KBqTJyvR3bzcTkP4As'
+sccess_secret='2M00eOoTvAiHjm8NXQYquWprfMgPv9pSRleMbFZlGDEGQ'
+
